6dd0f943be379e3a55593b09729c9b02f2a6da44
DESKTOP-U1LMLG9\Rob Laptop <robertmarshallproductions@gmail.com>
2019-03-23 21:23:10 +0000

    Added hero

---
+++ src/components/Hero/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import SkillList from '../SkillList'
+import './hero.scss'
+
+const Hero = () => (
+    <div className="hero">
+        <div className="container">
+            <h1 className="hero__title">
+                I'm Robert Marshall, a{' '}
+                <a href="https://github.com/robmarshall">developer</a> living in
+                Leeds, currently working for{' '}
+                <a href="https://www.fox.agency">Fox Agency</a>
+            </h1>
+            <SkillList />
+        </div>
+    </div>
+)
+
+export default Hero
