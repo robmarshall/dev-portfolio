f7c1a72585ee3ee3b0290bd699dc6acdb97a21d6
DESKTOP-U1LMLG9\Rob Laptop <robertmarshallproductions@gmail.com>
2019-03-24 11:27:32 +0000

    Added links and sort to skilllist

--- src/components/SkillList/index.js
+++ src/components/SkillList/index.js
@@ -1,31 +1,60 @@
 import React from 'react'
 import Color from 'color'
+import DynamicSort from '../../utils/helpers/DynamicSort'
 import './skilllist.scss'
 
 const SkillList = () => {
     const skills = [
-        { title: 'Sass', color: '#c6538c' },
-        { title: 'JavaScript', color: '#F7DF1E' },
-        { title: 'React', color: '#61dafb' },
-        { title: 'ES6', color: '#F7DF1E' },
-        { title: 'Node', color: '#026e00' },
-        { title: 'WordPress', color: '#207196' },
-        { title: 'Gulp', color: '#E14A4D' },
-        { title: 'NPM', color: '#C53635' },
-        { title: 'Git', color: '#000000' },
-        { title: 'WebPack', color: '#8ACFF3' },
+        { title: 'Sass', color: '#c6538c', link: 'https://sass-lang.com/' },
+        {
+            title: 'JavaScript',
+            color: '#F7DF1E',
+            link: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript',
+        },
+        { title: 'React', color: '#61dafb', link: 'https://reactjs.org/' },
+        {
+            title: 'ES6',
+            color: '#F7DF1E',
+            link: 'https://en.wikipedia.org/wiki/ECMAScript',
+        },
+        { title: 'Node', color: '#026e00', link: 'https://nodejs.org/en/' },
+        {
+            title: 'WordPress',
+            color: '#663399',
+            link: 'https://wordpress.org/',
+        },
+        {
+            title: 'Gatsby',
+            color: '#207196',
+            link: 'https://www.gatsbyjs.org/',
+        },
+        { title: 'Grav', color: '#977cad', link: 'https://getgrav.org/' },
+        { title: 'Gulp', color: '#E14A4D', link: 'https://gulpjs.com/' },
+        { title: 'NPM', color: '#C53635', link: 'https://www.npmjs.com/' },
+        {
+            title: 'Git',
+            color: '#000000',
+            link: 'https://github.com/robmarshall',
+        },
+        { title: 'WebPack', color: '#8ACFF3', link: 'https://webpack.js.org/' },
     ]
 
+    // Sort the list by title
+    skills.sort(DynamicSort('title'))
+
     let skillList = skills.map(item => (
-        <li
-            key={item.title}
-            style={{
-                backgroundColor: item.color,
-                color: getCorrectColour(item.color),
-            }}
-            className="skilllist__item"
-        >
-            {item.title}
+        <li key={item.title} className="skilllist__item">
+            <a
+                className="skilllist__item__link"
+                title={`External link to ${item.title}`}
+                href={item.link}
+                style={{
+                    backgroundColor: item.color,
+                    color: getCorrectColour(item.color),
+                }}
+            >
+                {item.title}
+            </a>
         </li>
     ))
 
