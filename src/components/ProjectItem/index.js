4ab26e914e59e9df71791e8f7a128b33a5fcc4c2
DESKTOP-U1LMLG9\Rob Laptop <robertmarshallproductions@gmail.com>
2019-03-26 22:21:01 +0000

    Added coloured skills

--- src/components/ProjectItem/index.js
+++ src/components/ProjectItem/index.js
@@ -2,12 +2,35 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import RootImage from '../RootImage'
 import _ from 'lodash'
+import Color from 'color'
 import './projectitem.scss'
 
 const ProjectItem = ({ name, desc, imageFileName, skills, link, source }) => {
+    const skillBank = {
+        html5: {
+            title: 'HTML5',
+            color: '#E34F26',
+        },
+        css3: {
+            title: 'CSS3',
+            color: '#005A9C',
+        },
+        sass: {
+            title: 'SASS',
+            color: '#c6538c',
+        },
+    }
+
     const skillList = skills.map(skill => (
-        <li key={_.kebabCase(skill)} className="project__skilllist__item">
-            {skill}
+        <li
+            key={_.kebabCase(skill)}
+            className="project__skilllist__item"
+            style={{
+                backgroundColor: skillBank[skill].color,
+                color: getCorrectColour(skillBank[skill].color),
+            }}
+        >
+            {skillBank[skill].title}
         </li>
     ))
 
@@ -48,6 +71,15 @@ ProjectItem.propTypes = {
     skills: PropTypes.array,
     link: PropTypes.string,
     source: PropTypes.string,
+}
+
+function getCorrectColour(backColour) {
+    let color = Color(backColour)
+    if (color.isLight(backColour)) {
+        return '#000'
+    } else {
+        return '#fff'
+    }
 }
 
 export default ProjectItem
