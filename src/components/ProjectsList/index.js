4b2a87ab77386cb8e05ce5fb9b34980fde8df3d5
DESKTOP-U1LMLG9\Rob Laptop <robertmarshallproductions@gmail.com>
2019-03-26 20:52:36 +0000

    Added scroll reveal

--- src/components/ProjectsList/index.js
+++ src/components/ProjectsList/index.js
@@ -1,56 +1,73 @@
 import React from 'react'
 import ProjectItem from '../ProjectItem'
 import _ from 'lodash'
+import ScrollReveal from 'scrollreveal'
 import './projectlist.scss'
 
-const ProjectList = () => {
-    const projects = [
-        {
-            name: 'Burtons Safes',
-            desc: 'sfsfdsf',
-            image: 'url',
-            skills: ['HTML5', 'CSS3', 'SASS'],
-            link: 'URL',
-            source: 'source',
-        },
-        {
-            name: 'Burtons Safesss',
-            desc: 'sfsfdsf',
-            image: 'url',
-            skills: ['HTML5', 'CSS3', 'SASS'],
-            link: 'URL',
-            source: 'source',
-        },
-        {
-            name: 'Burtons Safessss',
-            desc: 'sfsfdsf',
-            image: 'url',
-            skills: ['HTML5', 'CSS3', 'SASS'],
-            link: 'URL',
-            source: 'source',
-        },
-    ]
+class ProjectList extends React.Component {
+    constructor(props) {
+        super(props)
+    }
 
-    const projectItems = projects.map(project => (
-        <ProjectItem
-            key={_.kebabCase(project.name)}
-            name={project.name}
-            desc={project.desc}
-            imageFileName={project.image}
-            skills={project.skills || []}
-            link={project.link || ''}
-            source={project.source || ''}
-        />
-    ))
+    componentDidMount() {
+        import('scrollreveal').then(({ default: ScrollReveal }) => {
+            const sr = ScrollReveal().reveal('.project-item', {
+                distance: '50px',
+                origin: 'top',
+                delay: 250,
+            })
+        })
+    }
 
-    return (
-        <div className="projects-list">
-            <div className="tight-container">
-                <h2 className="projects-title">Featured Projects</h2>
-                <div className="projects-list__wrap">{projectItems}</div>
+    render() {
+        const projects = [
+            {
+                name: 'Burtons Safes',
+                desc: 'sfsfdsf',
+                image: 'url',
+                skills: ['HTML5', 'CSS3', 'SASS'],
+                link: 'URL',
+                source: 'source',
+            },
+            {
+                name: 'Burtons Safesss',
+                desc: 'sfsfdsf',
+                image: 'url',
+                skills: ['HTML5', 'CSS3', 'SASS'],
+                link: 'URL',
+                source: 'source',
+            },
+            {
+                name: 'Burtons Safessss',
+                desc: 'sfsfdsf',
+                image: 'url',
+                skills: ['HTML5', 'CSS3', 'SASS'],
+                link: 'URL',
+                source: 'source',
+            },
+        ]
+
+        const projectItems = projects.map(project => (
+            <ProjectItem
+                key={_.kebabCase(project.name)}
+                name={project.name}
+                desc={project.desc}
+                imageFileName={project.image}
+                skills={project.skills || []}
+                link={project.link || ''}
+                source={project.source || ''}
+            />
+        ))
+
+        return (
+            <div className="projects-list">
+                <div className="tight-container">
+                    <h2 className="projects-title">Featured Projects</h2>
+                    <div className="projects-list__wrap">{projectItems}</div>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default ProjectList
