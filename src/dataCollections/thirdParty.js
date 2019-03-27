6f8e7bb98fccdaa00ae99654623ea94ae58aeeb4
DESKTOP-U1LMLG9\Rob Laptop <robertmarshallproductions@gmail.com>
2019-03-27 08:46:22 +0000

    Broke third party into own list

---
+++ src/dataCollections/thirdParty.js
@@ -0,0 +1,80 @@
+const thirdParty = {
+    css3: {
+        title: 'CSS3',
+        color: '#005A9C',
+    },
+    es6: {
+        title: 'ES6',
+        color: '#F7DF1E',
+        link: 'https://en.wikipedia.org/wiki/ECMAScript',
+    },
+    gatbsy: {
+        title: 'Gatbsy',
+        color: '#663399',
+        link: 'https://www.gatsbyjs.org/',
+    },
+    grav: {
+        title: 'Grav',
+        color: '#977cad',
+        link: 'https://getgrav.org/',
+    },
+    github: {
+        title: 'Github',
+        color: '#000000',
+        link: 'https://github.com/robmarshall',
+    },
+    gulp: {
+        title: 'Gulp',
+        color: '#E14A4D',
+        link: 'https://gulpjs.com/',
+    },
+    graphql: {
+        title: 'Graph QL',
+        color: '#E535AB',
+    },
+    html5: {
+        title: 'HTML5',
+        color: '#E34F26',
+    },
+    js: {
+        title: 'JavaScript',
+        color: '#F7DF1E',
+        link: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript',
+    },
+    npm: {
+        title: 'NPM',
+        color: '#C53635',
+        link: 'https://www.npmjs.com/',
+    },
+    netlify: {
+        title: 'Netlify',
+        color: '#35B2BB',
+    },
+    node: {
+        title: 'Node',
+        color: '#026e00',
+        link: 'https://nodejs.org/en/',
+    },
+    react: {
+        title: 'React',
+        color: '#61dafb',
+        link: 'https://reactjs.org/',
+    },
+    sass: {
+        title: 'SASS',
+        color: '#c6538c',
+        link: 'https://sass-lang.com/',
+    },
+    webpack: {
+        title: 'WebPack',
+        color: '#8ACFF3',
+        link: 'https://webpack.js.org/',
+    },
+    wordpress: {
+        title: 'WordPress',
+        color: '#207196',
+        link: 'https://wordpress.org/',
+    },
+}
+
+export default thirdParty
