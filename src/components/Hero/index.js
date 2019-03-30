eaa781ac5b3d869eb9d4d04099a65e172a69f021
DESKTOP-U1LMLG9\Rob Laptop <robertmarshallproductions@gmail.com>
2019-03-30 20:38:37 +0000

    Added target blank and titles to linjs

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -6,10 +6,30 @@ const Hero = () => (
     <div className="hero">
         <div className="container">
             <h1 className="hero__title">
-                I'm <a href="https://twitter.com/robertmars">Robert Marshall</a>
-            , a <a href="https://github.com/robmarshall">fullstack developer</a>{' '}
+                I'm{' '}
+                <a
+                    href="https://twitter.com/robertmars"
+                    target="_blank"
+                    title="This link will open up Twitter in a new tab"
+                >
+                    Robert Marshall
+                </a>
+                , a{' '}
+                <a
+                    href="https://github.com/robmarshall"
+                    target="_blank"
+                    title="This link will open up GitHub in a new tab"
+                >
+                    fullstack developer
+                </a>{' '}
                 living in Leeds, currently working for{' '}
-                <a href="https://www.fox.agency">Fox Agency</a>
+                <a
+                    href="https://www.fox.agency"
+                    target="_blank"
+                    title="This link will open up the Fox Agency Website in a new tab"
+                >
+                    Fox Agency
+                </a>
             </h1>
             <SkillList />
         </div>
