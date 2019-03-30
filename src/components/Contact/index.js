f92090c8174fc5124321b26dcbacddc154cd06e9
DESKTOP-U1LMLG9\Rob Laptop <robertmarshallproductions@gmail.com>
2019-03-30 20:38:25 +0000

    Added contact section

---
+++ src/components/Contact/index.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import './contact.scss'
+
+const Contact = () => (
+    <div className="contact">
+        <div className="tight-container">
+            <div className="contact__email-link">
+                Want to work together?{' '}
+                <a
+                    href="mailto:robertmarshallproductions@gmail.com"
+                    className="contact__email-link__link"
+                    title="I am happy to talk though whatever you need. Pop me a message and lets have a chat!"
+                >
+                    Get in touch!
+                </a>
+            </div>
+            <div className="contact__sub-links">
+                <a
+                    href="https://github.com/robmarshall"
+                    className="contact__sub-links__link"
+                    target="_blank"
+                    title="This link will open a new tab to GitHub"
+                >
+                    GitHub
+                </a>
+                <a
+                    href="https://codepen.io/RobertMarshall"
+                    className="contact__sub-links__link"
+                    target="_blank"
+                    title="This link will open a new tab to CodePen"
+                >
+                    CodePen
+                </a>
+                <a
+                    href="https://www.linkedin.com/in/robert-marshall-86775883/"
+                    className="contact__sub-links__link"
+                    target="_blank"
+                    title="This link will open a new tab to LinkedIn"
+                >
+                    LinkedIn
+                </a>
+                <a
+                    href="mailto:robertmarshallproductions@gmail.com"
+                    className="contact__sub-links__link"
+                    title="Email Rob to have a chat!"
+                >
+                    Email
+                </a>
+            </div>
+        </div>
+    </div>
+)
+
+export default Contact
