0ba74807f7c5b8cea69e6dab3390746d6521d04c
DESKTOP-U1LMLG9\Rob Laptop <robertmarshallproductions@gmail.com>
2019-03-30 20:39:12 +0000

    Added blog and contact section

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,16 @@ import Layout from '../components/Layout'
 
 import Hero from '../components/Hero'
 import ProjectsList from '../components/ProjectsList'
+import TNSPostsList from '../components/TNSPostsList'
+import Contact from '../components/Contact'
 
 const IndexPage = () => (
     <Layout>
         <SEO title="Home" keywords={[``]} />
         <Hero />
         <ProjectsList />
+        <TNSPostsList />
+        <Contact />
     </Layout>
 )
 
