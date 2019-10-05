a4bba245715dc37bfb400ee16976100cda850925
robmarshall <robertmarshllproductions@gmail.com>
2019-10-05 13:23:29 +0100

    Added more skills

--- src/components/SkillList/index.js
+++ src/components/SkillList/index.js
@@ -1,57 +1,57 @@
-import React from 'react'
-import { OutboundLink } from 'gatsby-plugin-google-analytics'
+import React from "react";
+import { OutboundLink } from "gatsby-plugin-google-analytics";
 import {
-    GetCorrectTextColour,
-    GetAccessibleBackColor,
-} from '../../utils/helpers/SkillColor'
-import thirdParty from '../../dataCollections/thirdParty'
-import './skilllist.scss'
+  GetCorrectTextColour,
+  GetAccessibleBackColor
+} from "../../utils/helpers/SkillColor";
+import thirdParty from "../../dataCollections/thirdParty.json";
+import "./skilllist.scss";
 
 class SkillList extends React.Component {
-    render() {
-        const skills = [
-            'sass',
-            'js',
-            'react',
-            'es6',
-            'node',
-            'wordpress',
-            'gatsby',
-            'grav',
-            'gulp',
-            'git',
-            'webpack',
-        ]
+  render() {
+    const skills = [
+      "sass",
+      "js",
+      "react",
+      "es6",
+      "node",
+      "wordpress",
+      "gatsby",
+      "grav",
+      "gulp",
+      "git",
+      "webpack",
+      "jest",
+      "storybook"
+    ];
 
-        skills.sort()
+    skills.sort();
 
-        let skillList = skills.map(skill => (
-            <li key={thirdParty[skill].title} className="skilllist__item">
-                <OutboundLink
-                    className="skilllist__item__link"
-                    title={`External link to ${thirdParty[skill].title}`}
-                    href={thirdParty[skill].link}
-                    style={{
-                        backgroundColor: GetAccessibleBackColor(
-                            thirdParty[skill].color
-                        ),
-                        color: GetCorrectTextColour(thirdParty[skill].color),
-                    }}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    {thirdParty[skill].title}
-                </OutboundLink>
-            </li>
-        ))
+    let skillList = skills.map(skill => (
+      <li key={thirdParty[skill].title} className="skilllist__item">
+        <OutboundLink
+          className="skilllist__item__link"
+          title={`External link to ${thirdParty[skill].title}`}
+          href={thirdParty[skill].link}
+          style={{
+            backgroundColor: GetAccessibleBackColor(thirdParty[skill].color),
+            color: GetCorrectTextColour(thirdParty[skill].color)
+          }}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {thirdParty[skill].title}
+        </OutboundLink>
+      </li>
+    ));
 
-        return (
-            <div className="skilllist">
-                <p className="skilllist__title">I use:</p>
-                <ul className="skilllist__list">{skillList}</ul>
-            </div>
-        )
-    }
+    return (
+      <div className="skilllist">
+        <p className="skilllist__title">I use:</p>
+        <ul className="skilllist__list">{skillList}</ul>
+      </div>
+    );
+  }
 }
 
-export default SkillList
+export default SkillList;
